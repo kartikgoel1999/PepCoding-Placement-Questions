f1e304af43df04ade96556f34c655d6b3c906291
kartikgoel1999 <52077666+kartikgoel1999@users.noreply.github.com>
2020-07-31 22:01:16 +0530

    Add files via upload

---
+++ ZPattern.java
@@ -0,0 +1,12 @@
+//Print z pattern
+class ZPattern 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("*****");
+		System.out.println(" "+" "+" "+"*");
+		System.out.println(" "+" "+"*");
+		System.out.println(" "+"*");
+		System.out.println("*****");
+	}
+}
